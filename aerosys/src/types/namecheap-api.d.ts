0e9a57241f9d4a75fc2da6e904f0c8c033ff3f1e
Abdulrahman <30813584+AbdulrahmanM-2@users.noreply.github.com>
2026-04-07 15:47:09 +0300

    Add files via upload

--- aerosys/src/types/namecheap-api.d.ts
+++ aerosys/src/types/namecheap-api.d.ts
@@ -1,1 +1,12 @@
-
+declare module 'namecheap-api' {
+  export class Namecheap {
+    constructor(config: {
+      user: string;
+      apiUser: string;
+      apiKey: string;
+      clientIp: string;
+      useSandbox?: boolean;
+    });
+    apiCall(command: string, params: any): Promise<any>;
+  }
+}
